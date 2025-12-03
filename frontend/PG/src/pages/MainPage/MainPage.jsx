f28be529354a72508d46f285345b15a118df195d
jo sung bum <mini293920@naver.com>
2025-12-03 15:16:02 +0900

    Feat: 홈페이지 UI 및 이벤트 추가

---
+++ frontend/PG/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function MainPage() {
+  return (
+    <div style={{
+      height: '100vh',
+      backgroundColor: 'white',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: '30px'
+    }}>
+      메인 페이지 (내용 준비 중)
+    </div>
+  );
+}
